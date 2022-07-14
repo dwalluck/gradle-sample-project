11b86f6576b40054dccbbc9fa16fda139adbf1b4
David Walluck <dwalluck@redhat.com>
2022-07-13 22:12:51 -0400

    Add shadow jar containing asm, a gradle build

---
+++ subproject3/src/main/java/org/goots/gradle/sampleproject/sub3/Main.java
@@ -0,0 +1,9 @@
+package org.goots.gradle.sampleproject.sub3;
+
+import org.objectweb.asm.Opcodes;
+
+public class Main {
+	public static void main(String[]args) {
+		System.out.println(Opcodes.NOP);
+	}
+}
